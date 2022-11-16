f0f1fa38547a3a711bf89cadb633928424539035
Hanna Kostiana <anna.kostianaia@gmail.com>
2022-11-16 20:04:15 +0200

    Add to task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,14 +3,14 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+const simpleLightbox = require('simplelightbox');
 const itemContainer = document.querySelector(`.gallery`);
 
 function createGalleryItemMarkup(galleryItems) {
     return galleryItems
         .map(({ preview, original, description }) => {
             return `
-        <li>
+        <li class="image__item">
             <a class="gallery__item" href="${original}">
                 <img class="gallery__image" src="${preview}" alt="${description}" />
             </a>
@@ -30,10 +30,6 @@ var lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
     captionPosition: 'bottom',
-});
-
-lightbox.on('show.simplelightbox', function () {
-	evt.preventDefault();
 });
 
 console.log(galleryItems);
