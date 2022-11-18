ccd55b4dd4d73bb3d5dbfc2a0add125b314afc9a
AnnaKostyanaya <34882411+AnnaKostyanaya@users.noreply.github.com>
2022-11-18 03:04:22 +0000

    Deploying to gh-pages from @ AnnaKostyanaya/goit-js-hw-08@eaf54a09668208698ef20563be5e7fbbbb995127 🚀

--- 02-video.844fa26e.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#vimeo-player"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}));o.on("play",(function(e){}))}();
-//# sourceMappingURL=02-video.844fa26e.js.map
